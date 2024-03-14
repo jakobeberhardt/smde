a51e5075af747fde3d6f4de0f406e48d75f23b83
jakobeberhardt <jakob.eberhardt@mail.de>
2024-03-14 16:41:39 +0100

    c.) and d.)

--- 1-asign/main.R
+++ 1-asign/main.R
@@ -24,3 +24,28 @@ head(data)
 ### b) Create a dataset including only types of laptops: Ultrabook, Notebook and 2 in 1 Convertible. (5p) ###
 filtered_data <- data[data$TypeName %in% c("Ultrabook", "Notebook", "2 in 1 Convertible"), ]
 head(filtered_data)
+
+### c) Summarize the variables Weight, Price and the categorical variables in the new created data set. (10p) ###
+summary(filtered_data$Weight)
+summary(filtered_data$Price)
+
+table(filtered_data$Company)
+table(filtered_data$TypeName)
+table(filtered_data$Cpu_brand)
+table(filtered_data$Gpu_brand)
+table(filtered_data$Os)
+
+### d) Cross classify the variables type of computer and the touch screen indicator in a table. ###
+### Compute and interpret the conditional probability tables. (15p)                             ###
+# 1.)
+contingency_table <- table(filtered_data$TypeName, filtered_data$TouchScreen)
+contingency_table
+
+# P(TouchScreen | TypeName)
+conditional_prob_TypeName_to_TouchScreen <- prop.table(contingency_table, 1)  # Margin 1 is for rows (TypeName)
+
+# P(TypeName | TouchScreen)
+conditional_prob_TouchScreen_to_TypeName <- prop.table(contingency_table, 2)  # Margin 2 is for columns (TouchScreen)
+
+conditional_prob_TypeName_to_TouchScreen
+conditional_prob_TouchScreen_to_TypeName
