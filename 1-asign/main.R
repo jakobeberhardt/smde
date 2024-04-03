bb8d999f8b3f4c21b5e1b597ce197a88d7264058
StefanoPetrilli <stefanop_1999@hotmail.it>
2024-04-03 10:08:29 +0200

    replace plot with hist in questino f

--- 1-asign/main.R
+++ 1-asign/main.R
@@ -6,15 +6,15 @@
 file_path <- "./data/laptop_data_cleaned.csv"
 col_classes <- c("factor",    # Company
                  "factor",    # TypeName
-                 "factor",   # Ram
+                 "factor",    # Ram
                  "numeric",   # Weight
                  "numeric",   # Price
-                 "factor",   # TouchScreen
-                 "factor",   # Ips
+                 "factor",    # TouchScreen
+                 "factor",    # Ips
                  "numeric",   # Ppi
                  "factor",    # Cpu_brand
-                 "factor",   # HDD
-                 "factor",   # SSD
+                 "factor",    # HDD
+                 "factor",    # SSD
                  "factor",    # Gpu_brand
                  "factor")    # Os
 
@@ -31,7 +31,12 @@ summary(filtered_data$Price)
 
 table(filtered_data$Company)
 table(filtered_data$TypeName)
+table(filtered_data$Ram)
+table(filtered_data$TouchScreen)
+table(filtered_data$Ips)
 table(filtered_data$Cpu_brand)
+table(filtered_data$HDD)
+table(filtered_data$SSD)
 table(filtered_data$Gpu_brand)
 table(filtered_data$Os)
 
@@ -63,16 +68,23 @@ class(filtered_data$TouchScreen)
 # Yes they are both categorical
 
 chisq.test(filtered_data$TypeName, filtered_data$TouchScreen)
-# Yes, we observe a p-value lower than alpha (0.05) so we have enough evidence to reject the null hypothesis, 
+# Yes, we observe a p-value lower than alpha (0.05) so we have enough evidence to reject the null hypothesis,
 # so we can state that there is an association.
 
 ### f) Check the distribution of Price first for all observations then for subgroups of type of laptop in ###
 ### the data set created in section (b). Does it follow a normal distribution? (15p)
 
-plot(data$Price)
-plot(filtered_data$Price)
-# We could state by the plots that the data does not follow a normal distribution. But to be sure we can preform a 
-# Shapiro wilk normality test 
+hist(data$Price,
+     breaks = seq(min(data$Price), max(data$Price), length.out = 100),
+     xlab = "Price",
+     main = "Distribution of Prices")
+hist(filtered_data$Price,
+     breaks = seq(min(filtered_data$Price), max(filtered_data$Price), length.out = 100),
+     xlab = "Price",
+     main = paste("Distribution of Prices for Filtered Dataset"))
+
+# The histogram plots suggest that neither distribution has a clear bell shape typical of the normal distribution. 
+# As a visual analysis might be misleading we can also use the Shapiro-Wilk statistical test
 
 shapiro.test(data$Price)
 shapiro.test(filtered_data$Price)
@@ -80,23 +92,28 @@ shapiro.test(filtered_data$Price)
 # In both datasets we have enough evidence to reject the null hypothesis, so we can state that the distribution is not normal.
 
 ### g) Create a data frame just by including Ultrabooks and Notebooks. (5p) ###
-ultra_note_df <- subset(data, TypeName %in% c("Ultrabook", "Notebook"))
+ultra_note_df <- droplevels(subset(data, TypeName %in% c("Ultrabook", "Notebook")))
 
 ### h) Make a boxplot to show the distribution of Price across two categories of Type: Ultrabook vs.
 ### Notebook. Interpret it. (10p) ###
-boxplot(Price ~ TypeName, ultra_note_df)
+boxplot(Price ~ TypeName, data = ultra_note_df,
+        xlab = "Type of Computer",
+        ylab = "Price",
+        main = "Distribution of Prices Across Ultrabooks and Notebooks")
 # We can tell by the boxplot shown that Ultrabook is significantly more expensive than Notebook
 
 ### i) Compare the average price of Ultrabooks and Notebooks by using the appropriate method. Do
 ### not forget to test the assumptions. (25p)  ###
-# When comparing the averages of two variables with non-normally distributed data, you can use non-parametric tests which 
+# When comparing the averages of two variables with non-normally distributed data, you can use non-parametric tests which
 # do not rely on the assumption of normality. One such test is the Wilcoxon test, with assumptions:
-
-# 1.dunno about assumptions
+# 1 - The observations in each group should be independent.
+#     This is the case of the dataset analyzed.
+# 2 - The population distribution of the difference scores is symmetric
+#     TODO test this
 
 only_ultra <- subset(data, TypeName == "Ultrabook")
 only_note <- subset(data, TypeName == "Notebook")
 wilcox.test(only_ultra$Price, only_note$Price)
 
-# With this test we have enough evidence to reject the null hypothesis as the p-value (2.2e-16) is lower than alpha. 
+# With this test we have enough evidence to reject the null hypothesis as the p-value (2.2e-16) is lower than alpha.
 # We can state that there is a significant difference between both means.
