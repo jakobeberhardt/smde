7332f6a1c596a0267f4a4205f1d4f4e8d8c9d722
StefanoPetrilli <stefanop_1999@hotmail.it>
2024-04-10 19:29:32 +0200

    finish first question

--- 1-asign/main.R
+++ 1-asign/main.R
@@ -2,7 +2,9 @@
 ### SMDE Assignment 1 Part 1 ###
 ################################
 
-### a) Import data set to R assigning the type of each variable correctly. (5p) ###
+
+
+### a) Import data set to R assigning the type of each variable correctly.
 file_path <- "./data/laptop_data_cleaned.csv"
 col_classes <- c("factor",    # Company
                  "factor",    # TypeName
@@ -21,11 +23,19 @@ col_classes <- c("factor",    # Company
 data <- read.csv(file_path, colClasses = col_classes)
 head(data)
 
-### b) Create a dataset including only types of laptops: Ultrabook, Notebook and 2 in 1 Convertible. (5p) ###
-filtered_data <- data[data$TypeName %in% c("Ultrabook", "Notebook", "2 in 1 Convertible"), ]
+### b) Create a dataset including only types of laptops:
+###    - Ultrabook
+###    - Notebook
+###    - 2 in 1 Convertible
+
+filtered_data <- data[data$TypeName %in% c("Ultrabook",
+                                           "Notebook",
+                                           "2 in 1 Convertible"), ]
 head(filtered_data)
 
-### c) Summarize the variables Weight, Price and the categorical variables in the new created data set. (10p) ###
+### c) Summarize the variables Weight, Price and the categorical variables
+###    in the new created data set.
+
 summary(filtered_data$Weight)
 summary(filtered_data$Price)
 
@@ -40,80 +50,136 @@ table(filtered_data$SSD)
 table(filtered_data$Gpu_brand)
 table(filtered_data$Os)
 
-### d) Cross classify the variables type of computer and the touch screen indicator in a table. ###
-### Compute and interpret the conditional probability tables. (15p)                             ###
-# 1.)
+### d) Cross classify the variables type of computer and the touch screen
+###    indicator in a table.
+###    Compute and interpret the conditional probability tables.
 contingency_table <- table(filtered_data$TypeName, filtered_data$TouchScreen)
 contingency_table
 
 # P(TouchScreen | TypeName)
-conditional_prob_TypeName_to_TouchScreen <- prop.table(contingency_table, 1)  # Margin 1 is for rows (TypeName)
+# Margin 1 is for rows (TypeName)
+conditional_prob_TypeName_to_TouchScreen <- prop.table(contingency_table, 1)
 
 # P(TypeName | TouchScreen)
-conditional_prob_TouchScreen_to_TypeName <- prop.table(contingency_table, 2)  # Margin 2 is for columns (TouchScreen)
+# Margin 2 is for columns (TouchScreen)
+conditional_prob_TouchScreen_to_TypeName <- prop.table(contingency_table, 2)
 
 conditional_prob_TypeName_to_TouchScreen
 conditional_prob_TouchScreen_to_TypeName
 
-# 2.) It is really likely that if you buy a "2 in 1 Convertible" it will have a TouchScreen enabled,
-# whereas Ultrabook and Notebook follow the oposite trend, specially in Notebook.
+# Obeserving the results obtained we can say that
+# it is really likely that if you buy a "2 in 1 Convertible" it will have a
+# TouchScreen available, whereas Ultrabooks and Notebooks follow the opposite
+# trend, specially in Notebooks.
 
-### e) Is there an association between the type of computer and the touch screen characteristic of a ###
-### computer. Analyze it by using proper statistical method. (10 p)                                  ###
+### e) Is there an association between the type of computer and the touch
+###    screen characteristic of a computer.
+###    Analyze it by using proper statistical method.
 
-# In order to test this we could use a chi square test, the assumption we take for this test is:
-# Assumption 1: Both variables are categorical.
+# To test an asssociation between the type of computer and the presence of a
+# touch screen we can use a chi square test.
+# The assumption we take for this test is that both variables are categorical.
 class(filtered_data$TypeName)
 class(filtered_data$TouchScreen)
 # Yes they are both categorical
 
 chisq.test(filtered_data$TypeName, filtered_data$TouchScreen)
-# Yes, we observe a p-value lower than alpha (0.05) so we have enough evidence to reject the null hypothesis,
-# so we can state that there is an association.
+# We observe a p-value (2.2e-16) lower than alpha (0.05) so we have enough
+# evidence to reject the null hypothesis, so we can state that there is an
+# association.
 
-### f) Check the distribution of Price first for all observations then for subgroups of type of laptop in ###
-### the data set created in section (b). Does it follow a normal distribution? (15p)
+### f) Check the distribution of Price first for all observations then for
+###    subgroups of type of laptop in the data set created in section (b).
+###    Does it follow a normal distribution? (15p)
 
+# All data
 hist(data$Price,
      breaks = seq(min(data$Price), max(data$Price), length.out = 100),
      xlab = "Price",
      main = "Distribution of Prices")
-hist(filtered_data$Price,
-     breaks = seq(min(filtered_data$Price), max(filtered_data$Price), length.out = 100),
-     xlab = "Price",
-     main = paste("Distribution of Prices for Filtered Dataset"))
-
-# The histogram plots suggest that neither distribution has a clear bell shape typical of the normal distribution. 
-# As a visual analysis might be misleading we can also use the Shapiro-Wilk statistical test
-
 shapiro.test(data$Price)
-shapiro.test(filtered_data$Price)
 
-# In both datasets we have enough evidence to reject the null hypothesis, so we can state that the distribution is not normal.
+# Regarding all the observations, the hisograms does not present a bell shape
+# and Shapiro-Wilk confirms that it's not normal.
 
-### g) Create a data frame just by including Ultrabooks and Notebooks. (5p) ###
-ultra_note_df <- droplevels(subset(data, TypeName %in% c("Ultrabook", "Notebook")))
+filtered_ultra <- subset(data, TypeName == "Ultrabook")
+filtered_notebook <- subset(data, TypeName == "Notebook")
+filtered_2in1 <- subset(data, TypeName == "2 in 1 Convertible")
 
-### h) Make a boxplot to show the distribution of Price across two categories of Type: Ultrabook vs.
-### Notebook. Interpret it. (10p) ###
+# Filtered data - Ultrabook
+hist(filtered_ultra$Price,
+     breaks = seq(min(filtered_ultra$Price),
+                  max(filtered_ultra$Price),
+                  length.out = 100),
+     xlab = "Price",
+     main = paste("Distribution of Prices for Ultrabooks in Filtered Dataset"))
+shapiro.test(filtered_ultra$Price)
+
+# Filtered data - Notebooks
+hist(filtered_notebook$Price,
+     breaks = seq(min(filtered_notebook$Price),
+                  max(filtered_notebook$Price),
+                  length.out = 100),
+     xlab = "Price",
+     main = paste("Distribution of Prices for Notebooks Filtered Dataset"))
+shapiro.test(filtered_notebook$Price)
+
+# The price for both Ultrabooks and Notebooks is normal.
+# We checked this both visually, checking that the graph present a bell shape,
+# and using the shaprio test in which they presenter a p-value grater than 0.05
+
+# Filtered data - 2 in 1 Convertible
+hist(filtered_2in1$Price,
+     breaks = seq(min(filtered_2in1$Price),
+                  max(filtered_2in1$Price),
+                  length.out = 100),
+     xlab = "Price",
+     main = paste("Distribution of Prices for 2 in 1 Convertible in Filtered Dataset"))
+shapiro.test(filtered_2in1$Price)
+
+# In the case of 2 in 1 Convertible the visual analysis already hinted a non
+# normal distribution.
+# Also the shaprio test confirms this as we rejected the null hypothesis with a
+# p-value smaller than 0.05 (p-value = 0.001445)
+
+### g) Create a data frame just by including Ultrabooks and Notebooks.
+ultra_note_df <- droplevels(subset(data, TypeName %in% c("Ultrabook",
+                                                         "Notebook")))
+
+### h) Make a boxplot to show the distribution of Price across two categories
+###    of Type: Ultrabook vs. Notebook. Interpret it.
 boxplot(Price ~ TypeName, data = ultra_note_df,
         xlab = "Type of Computer",
         ylab = "Price",
         main = "Distribution of Prices Across Ultrabooks and Notebooks")
-# We can tell by the boxplot shown that Ultrabook is significantly more expensive than Notebook
+# We can tell by the shown boxplot that Ultrabook is on average more
+# expensive than Notebook.
+# We can see that in both categories there are outliers. In the case of
+# Notebook, the outliers are more expensive while in the case of Ultrabooks the
+# only outlier is cheaper.
 
-### i) Compare the average price of Ultrabooks and Notebooks by using the appropriate method. Do
-### not forget to test the assumptions. (25p)  ###
-# When comparing the averages of two variables with non-normally distributed data, you can use non-parametric tests which
-# do not rely on the assumption of normality. One such test is the Wilcoxon test, with assumptions:
-# 1 - The observations in each group should be independent.
-#     This is the case of the dataset analyzed.
-# 2 - The population distribution of the difference scores is symmetric
-#     TODO test this
+### i) Compare the average price of Ultrabooks and Notebooks by using the
+###    appropriate method. Do not forget to test the assumptions.
 
-only_ultra <- subset(data, TypeName == "Ultrabook")
-only_note <- subset(data, TypeName == "Notebook")
-wilcox.test(only_ultra$Price, only_note$Price)
+# To compare the average price of data that are normally distributed we can
+# use the t-test.
+# The assumptions for t-tests are:
+# 1 - The data is continuous
+#     This is the case for the price.
+# 2 - Homogenity of variance
+#     To test this property we use the f-test (which also assumes normality)
 
-# With this test we have enough evidence to reject the null hypothesis as the p-value (2.2e-16) is lower than alpha.
-# We can state that there is a significant difference between both means.
+var.test(filtered_ultra$Price,
+         filtered_notebook$Price)
+
+#     As the p-value is 3.784e-13 we can reject the null hypotesis and state
+#     the variances are different. As we can also see this from the boxplot in
+#     section (1.h). Therefore, we need to apply the Welch t-test by using the
+#     option: var.equal = FALSE
+# 3 - Normality
+#     We already checked in point (1.f) that the data prices data for Notebooks
+#     and Ultrabooks are normally distributed.
+
+t.test(filtered_ultra$Price,
+       filtered_notebook$Price,
+       var.equal = FALSE)
