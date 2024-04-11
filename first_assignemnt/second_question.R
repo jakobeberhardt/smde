ff966428fe30500ed9e55f9a23bd723e53dff3ee
Seret1 <74184233+Seret1@users.noreply.github.com>
2024-04-11 13:51:34 +0200

    done_pol_to_check

--- first_assignemnt/second_question.R
+++ first_assignemnt/second_question.R
@@ -50,54 +50,94 @@ shapiro.test(filtered_data$Weight)
 # From the tests we have executed we can state that the Price follows a normal
 # distribution (p-val = 0.28) and Weight does not (p-val = 2.2e-16).
 
-### b) The objective is to analyze the relationship between the brand of the
-###    computer and its price and its weight. First test the assumptions of the
-###    statistical method by using corresponding test and plots. Write your
-###    conclusions about the assumptions. If there is any violated assumption,
-###    interpret what should be done? Are both variables suitable for the
-###    corresponding analysis? If not, explain why?
+### b) The objective is to analyze the relationship between the brand of the computer and its price
+### and its weight. First test the assumptions of the statistical method by using corresponding test
+### and plots. Write your conclusions about the assumptions. If there is any violated assumption,
+### interpret what should be done? Are both variables suitable for the corresponding analysis? If
+### not, explain why? (30p) ###
 
-# In order to analyze the relationship between the brand of the computer and
-# its price and its weight we can use a linear regresion model, the assumptions
-# for this model are:
-# 1 - Linearity
-#     The relationship between the independent variable(s) and the dependent
-#     variable should be linear.
-# 2 - Homoscedasticity
-#     The variance of the residuals should be constant across all levels of the
-#     independent variable(s).
-# 3 - Independence of the residuals.
-# 4 - Normality of Residuals
-#     The residuals (the differences between observed and predicted values) should
-#     be normally distributed.
+# In order to analyze the relationship between the brand of the computer and its price and its weight we can use an ANOVA,
+# the assumptions for this model are:
+# The distribution of the population must be normal.
+# Homoscedasticity: The variance of the residuals should be constant across all levels of the independent variable(s).
+# Observations should be independent of each other.
 
-# Linear model
-model <- lm(filtered_data$Price ~ filtered_data$Company)
+# We have seen that weight variable is not normally distributed, we will try to remove the outliers and do a log-transformation.
+# In order to remove the outliers we will follow the interquartile range (IQR) method along with the 1.5 IQR rule
 
-# Assumptions check:
+# Calculate first and third quartiles
+Q1 <- quantile(filtered_data$Weight, 0.25)
+Q3 <- quantile(filtered_data$Weight, 0.75)
 
-# Linearity
-plot(filtered_data$Company, filtered_data$Price)
+# Calculate interquartile range (IQR)
+IQR <- Q3 - Q1
 
-# Homoscedasticity
-bptest(model)
+# Define lower and upper bounds for outliers
+lower_bound <- Q1 - 1.5 * IQR
+upper_bound <- Q3 + 1.5 * IQR
 
-# We fail to reject the null as the p-val > 0.05, thus we can state that the variance of the error term is constant.
+# Identify outliers
+outliers <- which(filtered_data$Weight < lower_bound | filtered_data$Weight > upper_bound)
 
-# Calculate the residuals
-residuals <- residuals(model)
-# Perform ANOVA using the residuals
-anova_result <- anova(lm(residuals ~ 1, data = data))  # Compare the residuals to a model with only an intercept term
-# Print ANOVA table
-print(anova_result)# Plot residuals against fitted values
+# Remove outliers
+cleaned_data <- filtered_data[-outliers,]
 
-plot(model$fitted.values, abs(resid(model)), ylab = "Absolute Residuals", xlab = "Fitted Values", main = "Scale-Location Plot")
+# Normality test for the clean data
+shapiro.test(cleaned_data$Weight)
+# It is still not normal
+
+# Log-transformation
+log_clean_data <- log10(cleaned_data$Weight)
+
+# Normality test for the clean and log-transformed data
+shapiro.test(log_clean_data)
+
+# As the distribution of weight is still not normal we can not proceed with the analysis for the weight variable. 
+
+### c) After fulfilling the assumptions, apply the related statistical method and interpret your
+### findings. Does the brand of the computer have significant effect on its price and its weight?
+### (just consider the variable(s) that fulfill the assumptions tested in section (b)) (35p)
+
+# ANOVA
+model <- aov(filtered_data$Price ~ filtered_data$Company)
+summary(model)
+
+# We can apply a TukeyHSD post-hoc test in order to check which and how groups differ from each other
+TukeyHSD(model)
+
+# According to Tukey's test:
+# Dell has a significantly higher price compared to Acer, with a mean difference of approximately 0.623 (95% CI: [0.476, 0.769]) and an adjusted p-value of 0.000.
+# HP also has a significantly higher price compared to Acer, with a mean difference of approximately 0.503 (95% CI: [0.356, 0.651]) and an adjusted p-value of 0.000.
+# However, there is no significant difference in price between HP and Dell, as the mean difference is approximately -0.119 (95% CI: [-0.227, -0.012]) with an adjusted p-value of 0.025.
+
+# We can also do a couple post-hoc tests that adjust the p-value in order to reduce type I errors, bonferroni is more conservative than fdr. 
+pairwise.t.test(filtered_data$Price, filtered_data$Company, p.adjust.method = "bonferroni")
+pairwise.t.test(filtered_data$Price, filtered_data$Company, p.adjust.method = "fdr")
+
+# Both tests show that:
+# Acer vs. Dell: The p-value for comparing prices between Acer and Dell is extremely small (< 2e-16), which indicates a highly significant difference in prices between these two companies. 
+# Acer vs. HP: The p-value for comparing prices between Acer and HP is also very small, indicating a highly significant difference in prices between these two companies.
+# Dell vs. HP: The p-value for comparing prices between Dell and HP is 0.027, which is smaller than alpha (0.05), indicating a statistically significant difference in prices between these two companies, 
+# but not as extreme as the differences observed between Acer and each of Dell and HP.
+
+# In order to check homoscedasticity we can use a qqplot:
+plot(model, 2)
+# According to this plot we could state that the residuals distribution is normal, however we can further check this with a Levene's test.
+
+library(car)
+leveneTest(model)
+# The p-value obtained is 0.067, as it is greater than alpha we fail to reject the null hypothesis of homoscedasticity, so the assumtion is fulfilled.
+
+### d) Analyze the effect of brand and touch screen characteristics together on the price. Analyze
+### whether the interaction of two term is significant. Interpret your findings. (Do not forget to
+### confirm the assumptions!) (25 p)
+
+model_interaction <- aov(filtered_data$Price ~ filtered_data$Cpu_brand*filtered_data$TouchScreen)
+
+summary(model_interaction)
+
+# As we can see through the summary of model, brand and touchscreen have a highly significant effect (***) on the price.
 
 
-# Normality of Residuals
-# Visually we can check it with a QQ-plot, if the residuals follow the diagonal the distribution is normal
-plot(model, which = 2)
-# We can also test it with a shapiro test as before
-shapiro.test(model)
 
 # Both tests conclude that the residuals follow a normal distribution
