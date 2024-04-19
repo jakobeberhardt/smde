0259a5f8dc1089d3b1234bc07a1a4ab6f4f2b040
Seret1 <74184233+Seret1@users.noreply.github.com>
2024-04-19 16:12:38 +0200

    Update third_question.R

--- first_assignemnt/third_question.R
+++ first_assignemnt/third_question.R
@@ -11,7 +11,7 @@ col_classes <- c("factor",    # Company
                  "numeric",   # Ppi
                  "factor",    # Cpu_brand
                  "factor",   # HDD
-                 "factor",   # SSD
+                 "numeric",   # SSD
                  "factor",    # Gpu_brand
                  "factor")    # Os
 
@@ -28,82 +28,155 @@ data <- read.csv("Downloads/laptop_data_cleaned.csv", colClasses = col_classes)
 # coefficients of the model (25p)
 
 # The assumptions we need to take into account for a simple linear regression model are:
-# Linearity: The relationship between the independent variable(s) and the dependent variable should be linear.
+# No multicolinearity: Not correlation between independent variables.
 # Homoscedasticity: The variance of the residuals should be constant across all levels of the independent variable(s).
 # Normality of Residuals: The residuals (the differences between observed and predicted values) should be normally distributed.
 
 # In order to check which model is better we can check the Adjusted R-squared using summary(). The R-squared value is the amount 
 # of variance explained by your model. It is a measure of how well your model fits your data. As a matter of fact, the higher it is, the better is your model.
 # Moreover adjusted R-squared will decrease if additional variables do not contribute to the model’s explanatory power.
-# We have three numerical variables (Price, Weight and Ppi) so there are few models we can test: 
+# We have four numerical variables (Price, Weight, SSD and Ppi) so there are few models we can test: 
 
-# Only Weight
+### Only Weight
 ow_model <- lm(Price ~ Weight, data = data)
 plot(ow_model)
 summary(ow_model)
 # Adjusted R-squared: 0.02215
 
-# Assumptions check 
+## Assumptions check (we do not have to check multicolinearity as we are only using one independent variable)
 
-# Linearity: as p-value of weight in the Linear Model is significant (***), we can state that there is a linear correlation between Price and Weight.
-# Homoscedasticity:
+## Homoscedasticity:
 bptest(ow_model)
 # As the p-val<0.05 we have enough evidence for rejecting the null hypothesis, so we can state that the Homoscedasticity assumption is not fulfilled.
 
+## Normality of the residuals
 plot(ow_model, 2)
 shapiro.test(ow_model$residuals)
+# As the pval(3.73e-09)<0.05 we have enough evidence to reject the null hypothesis, the assumption of normality of the residuals is not fulfilled.
 
-
-# Only Ppi
+### Only Ppi
 op_model <- lm(Price ~ Ppi, data = data)
 summary(op_model)
 # Adjusted R-squared: 0.2305
 
-# Assumptions check 
+## Assumptions check 
 
-# Linearity: as p-value of weight in the Linear Model is significant (***), we can state that there is a linear correlation between Price and Ppi.
-# Homoscedasticity:
+## Homoscedasticity:
 bptest(op_model)
 # As the p-val>0.05 we have enough evidence for accepting the null hypothesis, so we can state that the Homoscedasticity assumption is fulfilled.
 
-# In this case we can see that Ppi is a better predictor than Weight for Price.
+## Normality of the residuals
+plot(op_model, 2)
+shapiro.test(op_model$residuals)
+# In this case the p-val(0.223)>0.05 so we have enough evidence for accepting the null hypothesis, the assumption of normality in the residuals is fulfilled.
 
+### Only SSD
+os_model <- lm(Price ~ SSD, data = data)
+summary(os_model)
+# Adjusted R-squared: 0.4336
+
+## Homoscedasticity:
+bptest(os_model)
+# As the p-val>0.05 we have enough evidence for accepting the null hypothesis, so we can state that the Homoscedasticity assumption is fulfilled.
+
+## Normality of residuals
+plot(os_model, 2)
+shapiro.test(os_model$residuals)
+# With these tests we can be sure that residuals follow a normal distribution (p-val>0.05). 
+
+# In this case we can see that SSD is a better predictor than Ppi for Price. We do not take weight into account as it does not fulfill the assumptions and the results are not reliable.
 
 # b) Fit a multivariate linear regression model with two (numerical) independent variables. Choose
 # the most significant regression model with two predictors. (Transform the variables if it is
 # needed and test all the assumptions.) Then compare this model to the simple linear regression
 # model that you fit in (a). Which one is a better model? Why? (25p)
 
-# Weight + Ppi
-lm_model <- lm(Price ~ Weight + Ppi, data = data)
-summary(lm_model)
+### Weight + Ppi
+wp_model <- lm(Price ~ Weight + Ppi, data = data)
+summary(wp_model)
 # Adjusted R-squared: 0.3336
 
-# Assumptions check 
+## Assumptions check 
 
-# Linearity: as p-value of Weight, Ppi and the interaction in the Linear Model is significant (***), we can state that there is a linear correlation between Price and Weight.
-# Homoscedasticity:
-bptest(lm_model)
+## Homoscedasticity:
+bptest(wp_model)
 # As the p-val<0.05 we have enough evidence for rejecting the null hypothesis, so we can state that the Homoscedasticity assumption is not fulfilled.
 
-# Log(weight + Ppi)
-log_var <- log(data$Weight, data$Ppi)
-log_model <- lm(Price ~ log_var, data = data)
-summary(log_model)
-# Adjusted R-squared: -0.0007012
-
-# Assumptions check 
-
-# Linearity: as p-value of the logged variables in the Linear Model is not significant, we can state that there is not a linear correlation between Price and logged variables.
+## Normality of the residuals
+plot(wp_model, 2)
+shapiro.test(wp_model$residuals)
+# The assumption of normality of the residuals is not fulfilled as the p-val(0.0014)<0.05, the results of this model are not reliable.
 
 
-# With this values we could state that the best model for predicting Price is Weight+Ppi, but we can do a further checking with a partial F-test.
+### Weight + SSD
+ws_model <- lm(Price ~ Weight + SSD, data = data)
+summary(ws_model)
+# Adjusted R-squared: 0.409
 
+## Assumptions check 
 
+## Homoscedasticity:
+bptest(ws_model)
+# As the p-val(0.0324)<0.05 we have enough evidence for rejecting the null hypothesis, so we can state that the Homoscedasticity assumption is not fulfilled.
+
+### Ppi + SSD
+ps_model <- lm(Price ~ Ppi + SSD, data = data)
+summary(ps_model)
+# Adjusted R-squared: 0.4637
+
+## Assumptions check 
+
+## Homoscedasticity:
+bptest(ps_model)
+# As the p-val(0.2116)>0.05 we have enough evidence for accepting the null hypothesis, so we can state that the Homoscedasticity assumption is fulfilled.
+
+## Normality of the residuals
+plot(ps_model, 2)
+shapiro.test(ps_model$residuals)
+# The assumption of normality of the residuals is fulfilled as the p-val(0.1543)>0.05.
+
+## No Multicolinearity
+# In order to test correlation we can check the variance inflation factor
+vif(ps_model)
+# The correlation between Ppi and SSD is 1.33, we can state that the assumption of not multicolinearity between the independent variables (Ppi and SSD) is fulfilled
+
+# With this results we could state that the best model for predicting Price is Ppi+SSD, as it has the higher Adjusted R-squared. We can do a further checking with a partial F-test.
+# For the partial F-tests we will use ANOVA:
+
+anova(ps_model, os_model)
+# The ANOVA test suggests that Model 1 provides a significantly better fit to the data compared to Model 2.
+# This conclusion is based on the significantly lower residual sum of squares (RSS) and the associated F-statistic with 
+# a very low p-value (***), indicating strong evidence against the null hypothesis.
 
 # c) Now add a factor to the regression model you have chosen in section (b). (You can write a loop
 # to add factors one by one to the previous model and decide based on the results.) Interpret
 # the coefficients and overall summary of the model. Test the model in section (b) with the
 # model that has an additional factor. Which one would you choose? Why? (35p)
+
+# we have to do anova that is doing partial F-tests to compare single with full models
+# In ANOVA, the null hypothesis is that there is no difference among group means
+
+for (fact in c("Company", "TypeName", "Ram", "TouchScreen", "Ips", "Cpu_brand", "HDD", "Gpu_brand", "Os")) {
+  formula_str <- paste("Price ~ Ppi + SSD +", fact)  # Create the formula as a string
+  print(paste("Price ~ Ppi + SSD +", fact))
+  psf_model <- lm(as.formula(formula_str), data = data)  # Convert the string to a formula
+  print(summary(psf_model))  # Print the summary of the model
+}
+
+## Adjusted R-squared per factor
+# Company: 0.5471
+# TypeName: 0.632
+# Ram: 0.6842
+# TouchScreen: 0.4651
+# Ips: 0.4745
+# Cpu_brand: 0.6946
+# HDD: 0.5146
+# Gpu_brand: 0.5494
+# OS: 0.5056
+
+# With these results we could state that Cpu_brand is the factor that helps more to 
+
+
 # d) Test the validity of the final model. (15p)
+
 
