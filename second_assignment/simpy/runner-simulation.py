93d8d7825daed785a91aff708b0b97ccb5d000ed
Jan Herlyn <jan.herlyn@adevinta.com>
2024-06-06 18:47:32 +0200

    Simulation

--- second_assignment/simpy/runner-simulation.py
+++ second_assignment/simpy/runner-simulation.py
@@ -25,10 +25,22 @@ class MonitoredResource(simpy.Resource):
 
 
 class Runner(object):
-    def __init__(self, env, name, distances, queues, usage_times, need_functions, need_satisfaction, average_waiting_times, skip_if_waiting_time_higher):
+    def __init__(
+        self,
+        env,
+        name,
+        running_time,
+        distances,
+        queues,
+        usage_times,
+        need_functions,
+        need_satisfaction,
+        average_waiting_times,
+        skip_if_waiting_time_higher,
+    ):
         self.env = env
         # Start the run process everytime an instance is created.
-        self.action = env.process(self.run())
+        self.running_time = running_time
         self.distances = distances
         self.queues = queues
         self.usage_times = usage_times
@@ -40,16 +52,22 @@ class Runner(object):
         self.skip_if_waiting_time_higher = skip_if_waiting_time_higher
         self.average_waiting_times = average_waiting_times
         self.skipped_needs = {k: [] for k in need_functions.keys()}
-        self.current_needs =  {k: [] for k in need_functions.keys()}
+        self.current_needs = {k: [] for k in need_functions.keys()}
+        self.cummulative_usage_time = {k: 0 for k in usage_times.keys()}
+        self.stop_times = []
+
+        self.action = env.process(self.run())
 
     def run(self):
         # print("{} {} Start marathon".format( self.env.now, self.name))
         for i in range(0, len(self.distances)):
             # print("{} starting stage {}".format(self.env.now , i))
-            yield self.env.timeout(running_time(self.distances[i]))
+            yield self.env.timeout(self.running_time(self.distances[i]))
             yield from self.use_stations(i)
+        self.finishing_time = self.env.now
 
     def use_stations(self, index):
+        self.stop_times.append(self.env.now)
         for station in self.skipped_needs:
             self.skipped_needs[station].append(None)
         for station in self.needs:
@@ -61,21 +79,31 @@ class Runner(object):
         random.shuffle(keys)
 
         for station in keys:
-            yield from self.use_station(index,station)
+            yield from self.use_station(index, station)
 
     def use_station(self, index, station):
-        skip_due_to_queue_size = self.get_waiting_time(index, station) > self.skip_if_waiting_time_higher[station] and not self.needs[station] >= 1.0
-        if random.uniform(0, 1) < self.needs[station] and not skip_due_to_queue_size: # if we want to go but have to skip
+        skip_due_to_queue_size = (
+            self.get_waiting_time(index, station)
+            > self.skip_if_waiting_time_higher[station]
+            and not self.needs[station] >= 1.0
+        )
+        if (
+            random.uniform(0, 1) < self.needs[station] and not skip_due_to_queue_size
+        ):  # if we want to go but have to skip
             with self.queues[index][station].request() as stn:
                 yield stn
-                yield self.env.timeout(self.usage_times[station]())
+                usage_time = self.usage_times[station]()
+                self.cummulative_usage_time[station] += usage_time
+                yield self.env.timeout(usage_time)
             self.needs[station] = self.need_satisfaction[station](self.needs[station])
             self.previous_times[station] = self.env.now
         elif skip_due_to_queue_size:
             self.skipped_needs[station][index] = self.needs[station]
 
-    def get_waiting_time(self,index,station):
-        return len(self.queues[index][station].queue) * self.average_waiting_times[station]
+    def get_waiting_time(self, index, station):
+        return (
+            len(self.queues[index][station].queue) * self.average_waiting_times[station]
+        )
 
     def update_need(self, station):
         time_difference = self.env.now - self.previous_times[station]
@@ -83,48 +111,48 @@ class Runner(object):
         self.previous_times[station] = self.env.now
 
 
-mean_total_running_time = 11933.0
-running_time_stddev = 1200
-running_time = lambda distance: np.random.normal(
-    mean_total_running_time / 42.0 * distance, running_time_stddev / 42.0 * distance
-)
+marathon_distance = 42.0
 
 average_usage_times = {
-    "meal":  60,
-    "water": 30,
-    "toilet": 45,
-    "medical": 300,
+    "meal": 5,
+    "water": 5,
+    "toilet": 40,
+    "medical": 420,
 }
 
 skip_if_waiting_time_higher = {
-    "meal":  120,
+    "meal": 120,
     "water": 120,
     "toilet": 180,
     "medical": 600,
 }
 
-water_drinking_time = lambda: max(np.random.normal(30, 5), 5)
-meal_time = lambda: max(np.random.normal(60, 15), 10)
-toilet_time = lambda: max(np.random.normal(45, 10), 10)
-medical_time = lambda: max(np.random.normal(300, 100), 30)
-usage_times = {"water": water_drinking_time, "meal": meal_time, "toilet": toilet_time, "medical": medical_time}
+water_drinking_time = lambda: max(np.random.normal(average_usage_times["water"], 2), 1)
+meal_time = lambda: max(np.random.normal(average_usage_times["meal"], 2), 1)
+toilet_time = lambda: max(np.random.normal(average_usage_times["toilet"], 10), 5)
+medical_time = lambda: max(np.random.normal(average_usage_times["medical"], 100), 30)
 
-need_functions = {
-    "meal": lambda x: x / 3600.0 / 2.0,  # once per 2 hours
-    "water": lambda x: x / 3600.0 * 3.0,  # three times per hour
-    "toilet": lambda x: x / 3600.0 * 2.0,  # two times per hour
-    "medical": lambda x: x
-    / mean_total_running_time
-    / 5,  # over the course of the race, 1 in 5 runners needs medical attention
+usage_times = {
+    "water": water_drinking_time,
+    "meal": meal_time,
+    "toilet": toilet_time,
+    "medical": medical_time,
 }
 
-
+need_functions = {
+    "meal": lambda x: x / 3600.0,  # once per hour
+    "water": lambda x: x / 3600.0 * 3.0,  # three times per hour
+    "toilet": lambda x: x / 3600.0 / 2.0,  # once per 2 hours
+    "medical": lambda x: x
+    / 3600.0
+    / 1000,  # over the course of the race, 1 in 5 runners needs medical attention
+}
 
 need_satisfaction = {
-    "meal": lambda x: (x - 1.0,0),  # once per 3.5 hours
-    "water": lambda x: (x - 1.0, 0),  # three times per hour
-    "toilet": lambda x: x - 0.5,  # two times per hour
-    "medical": lambda x: x
+    "meal": lambda x: x - 1.0,
+    "water": lambda x: x - 1.0,
+    "toilet": lambda x: x - 1.0,
+    "medical": lambda x: x - 1.0,
 }
 
 # Resources
@@ -141,94 +169,135 @@ need_satisfaction = {
 # – Km 37,5: WC, Water, Isotonic drink, and Finsher Gel
 # – Km 40: WC, Water, Isotonic drink, Medical sevices and Banana
 
-def assign_resources(env, meal_capacity = 10, water_capacity = 10, toilet_capacity = 10, medical_capacity = 10):
-  return [
-      { # km 5
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-          "medical": MonitoredResource(env, medical_capacity),
-      },
-      {# km 10
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-          "medical": MonitoredResource(env, medical_capacity),
-      },
-      {# km 15
-          "meal": MonitoredResource(env, meal_capacity),
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-          "medical": MonitoredResource(env, medical_capacity),
-      },
-      {# km 20
-          "meal": MonitoredResource(env, meal_capacity),
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-          "medical": MonitoredResource(env, medical_capacity),
-      },
-      {# km 25
-          "meal": MonitoredResource(env, meal_capacity),
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-          "medical": MonitoredResource(env, medical_capacity),
-      },
-      {# km 27.5
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-      },
-      {# km 30
-          "meal": MonitoredResource(env, meal_capacity),
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-          "medical": MonitoredResource(env, medical_capacity),
-      },
-      {# km 32.5
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-      },
-      {# km 35
-          "meal": MonitoredResource(env, meal_capacity),
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-          "medical": MonitoredResource(env, medical_capacity),
-      },
-      {# km 37.5
-          "meal": MonitoredResource(env, meal_capacity),
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-      },
-      {# km 40
-          "meal": MonitoredResource(env, meal_capacity),
-          "water": MonitoredResource(env, water_capacity),
-          "toilet": MonitoredResource(env, toilet_capacity),
-          "medical": MonitoredResource(env, medical_capacity),
-      },
-  ]
-distances = [5.0, 5.0, 5.0, 5.0, 5.0, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5]
+
+def assign_resources(
+    env, meal_capacity=10, water_capacity=10, toilet_capacity=10, medical_capacity=10
+):
+    return [
+        {  # km 5
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+            "medical": MonitoredResource(env, medical_capacity),
+        },
+        {  # km 10
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+            "medical": MonitoredResource(env, medical_capacity),
+        },
+        {  # km 15
+            "meal": MonitoredResource(env, meal_capacity),
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+            "medical": MonitoredResource(env, medical_capacity),
+        },
+        {  # km 20
+            "meal": MonitoredResource(env, meal_capacity),
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+            "medical": MonitoredResource(env, medical_capacity),
+        },
+        {  # km 25
+            "meal": MonitoredResource(env, meal_capacity),
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+            "medical": MonitoredResource(env, medical_capacity),
+        },
+        {  # km 27.5
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+        },
+        {  # km 30
+            "meal": MonitoredResource(env, meal_capacity),
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+            "medical": MonitoredResource(env, medical_capacity),
+        },
+        {  # km 32.5
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+        },
+        {  # km 35
+            "meal": MonitoredResource(env, meal_capacity),
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+            "medical": MonitoredResource(env, medical_capacity),
+        },
+        {  # km 37.5
+            "meal": MonitoredResource(env, meal_capacity),
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+        },
+        {  # km 40
+            "meal": MonitoredResource(env, meal_capacity),
+            "water": MonitoredResource(env, water_capacity),
+            "toilet": MonitoredResource(env, toilet_capacity),
+            "medical": MonitoredResource(env, medical_capacity),
+        },
+        {},  # Finishing line
+    ]
 
 
-def run_simulation(usage_times, need_functions, meal_capacity = 10, water_capacity = 10, toilet_capacity = 10, medical_capacity= 10):
-  env = simpy.Environment()
-  resources = assign_resources(env)
-
-  # create runners
-  runners = [
-      Runner(
-          env,
-          name="runner %i" % i,
-          distances=distances,
-          queues=resources,
-          usage_times=usage_times,
-          need_functions=need_functions,
-          need_satisfaction = need_satisfaction,
-          average_waiting_times=average_usage_times,
-          skip_if_waiting_time_higher=skip_if_waiting_time_higher
-      )
-      for i in range(1, 8303)
-  ]
+distances = [5.0, 5.0, 5.0, 5.0, 5.0, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.0]
 
 
-  # running simulation
-  env.run()
-  return (runners, resources)
+def generate_running_function(mean_total_running_time, running_time_stddev):
+    expected_finishing_time = np.random.normal(
+        mean_total_running_time, running_time_stddev
+    )
+    return lambda distance: expected_finishing_time * distance / marathon_distance
 
-runners, resources = run_simulation(usage_times, need_functions)
+
+def run_simulation(
+    mean_total_running_time,
+    usage_times,
+    need_functions,
+    meal_capacity=10,
+    water_capacity=10,
+    toilet_capacity=10,
+    medical_capacity=10,
+):
+    env = simpy.Environment()
+    resources = assign_resources(
+        env,
+        meal_capacity=meal_capacity,
+        water_capacity=water_capacity,
+        toilet_capacity=toilet_capacity,
+        medical_capacity=medical_capacity,
+    )
+
+    running_time_stddev = 1200
+
+    # create runners
+    runners = []
+    for i in range(0, 8303):
+        runners.append(
+            Runner(
+                env,
+                name="runner %i" % i,
+                running_time=generate_running_function(
+                    mean_total_running_time, running_time_stddev
+                ),
+                distances=distances,
+                queues=resources,
+                usage_times=usage_times,
+                need_functions=need_functions,
+                need_satisfaction=need_satisfaction,
+                average_waiting_times=average_usage_times,
+                skip_if_waiting_time_higher=skip_if_waiting_time_higher,
+            )
+        )
+
+    # running simulation
+    env.run()
+    return (runners, resources)
+
+
+runners, resources = run_simulation(
+    11933.0,
+    usage_times,
+    need_functions,
+    meal_capacity=40,
+    water_capacity=80,
+    toilet_capacity=45,
+    medical_capacity=5,
+)
